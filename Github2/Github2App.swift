b6109a783238563f4459666f7eb9bd7e984a423c
Ashley Murrell <amurrel5@vols.utk.edu>
2023-07-12 21:27:28 -0500

    Initial Commit

---
+++ Github2/Github2App.swift
@@ -0,0 +1,17 @@
+//
+//  Github2App.swift
+//  Github2
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
